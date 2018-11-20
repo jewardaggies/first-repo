4733472ae0b4b69628afc57063795ecd564a5ee0
Jon Ward <wardjonjon6@gmail.com>
2018-11-20 11:02:15 -0500

    second commit

---
+++ Goodbye163.java
@@ -0,0 +1,14 @@
+/**
+ * Simple clas that prints out "Goodbye COMP 163".
+ * @author jeward@aggies.ncat.edu
+ */
+public class Goodbye163 {
+  /**
+   * Main method to print "Goodbye COMP 163".
+   * @param args Ignored
+   */
+  public static void main(String[] args) {
+    System.out.println("Goodbye COMP 163");
+  }
+
+}
